d24ad615afbec55b44a3032541d15d14d1b7d602
danielk9538 <115681045+danielk9538@users.noreply.github.com>
2023-12-06 16:52:04 +0000

    Wed, Dec 6, 2023, 11:52 AM -05:00

--- kaboom/mygame/src/main.js
+++ kaboom/mygame/src/main.js
@@ -88,63 +88,60 @@ kaboom({
 	background: [141, 183, 255],
 })
 
+// // custom component controlling enemy patrol movement
+// function patrol(speed = 60, dir = 1) {
+// 	return {
+// 		id: "patrol",
+// 		require: [ "pos", "area" ],
+// 		add() {
+// 			this.on("collide", (obj, col) => {
+// 				if (col.isLeft() || col.isRight()) {
+// 					dir = -dir
+// 				}
+// 			})
+// 		},
+// 		update() {
+// 			this.move(speed * dir, 0)
+// 		},
+// 	}
+// }
 
-setGravity(3200)
-
-// custom component controlling enemy patrol movement
-function patrol(speed = 60, dir = 1) {
-	return {
-		id: "patrol",
-		require: [ "pos", "area" ],
-		add() {
-			this.on("collide", (obj, col) => {
-				if (col.isLeft() || col.isRight()) {
-					dir = -dir
-				}
-			})
-		},
-		update() {
-			this.move(speed * dir, 0)
-		},
-	}
-}
-
-// custom component that makes stuff grow big
-function big() {
-	let timer = 0
-	let isBig = false
-	let destScale = 1
-	return {
-		// component id / name
-		id: "big",
-		// it requires the scale component
-		require: [ "scale" ],
-		// this runs every frame
-		update() {
-			if (isBig) {
-				timer -= dt()
-				if (timer <= 0) {
-					this.smallify()
-				}
-			}
-			this.scale = this.scale.lerp(vec2(destScale), dt() * 6)
-		},
-		// custom methods
-		isBig() {
-			return isBig
-		},
-		smallify() {
-			destScale = 1
-			timer = 0
-			isBig = false
-		},
-		biggify(time) {
-			destScale = 2
-			timer = time
-			isBig = true
-		},
-	}
-}
+// // custom component that makes stuff grow big
+// function big() {
+// 	let timer = 0
+// 	let isBig = false
+// 	let destScale = 1
+// 	return {
+// 		// component id / name
+// 		id: "big",
+// 		// it requires the scale component
+// 		require: [ "scale" ],
+// 		// this runs every frame
+// 		update() {
+// 			if (isBig) {
+// 				timer -= dt()
+// 				if (timer <= 0) {
+// 					this.smallify()
+// 				}
+// 			}
+// 			this.scale = this.scale.lerp(vec2(destScale), dt() * 6)
+// 		},
+// 		// custom methods
+// 		isBig() {
+// 			return isBig
+// 		},
+// 		smallify() {
+// 			destScale = 1
+// 			timer = 0
+// 			isBig = false
+// 		},
+// 		biggify(time) {
+// 			destScale = 2
+// 			timer = time
+// 			isBig = true
+// 		},
+// 	}
+// }
 
 // define some constants
 const JUMP_FORCE = 1320
