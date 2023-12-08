b630f13ccbc62135fa595b2c2c08560d657a78c2
danielk9538 <115681045+danielk9538@users.noreply.github.com>
2023-12-08 15:19:44 +0000

    Fri, Dec 8, 2023, 10:19 AM -05:00

--- kaboom/mygame/src/main.js
+++ kaboom/mygame/src/main.js
@@ -103,43 +103,6 @@ k.add([
 // 	}
 // }
 
-// // custom component that makes stuff grow big
-// function big() {
-// 	let timer = 0
-// 	let isBig = false
-// 	let destScale = 1
-// 	return {
-// 		// component id / name
-// 		id: "big",
-// 		// it requires the scale component
-// 		require: [ "scale" ],
-// 		// this runs every frame
-// 		update() {
-// 			if (isBig) {
-// 				timer -= dt()
-// 				if (timer <= 0) {
-// 					this.smallify()
-// 				}
-// 			}
-// 			this.scale = this.scale.lerp(vec2(destScale), dt() * 6)
-// 		},
-// 		// custom methods
-// 		isBig() {
-// 			return isBig
-// 		},
-// 		smallify() {
-// 			destScale = 1
-// 			timer = 0
-// 			isBig = false
-// 		},
-// 		biggify(time) {
-// 			destScale = 2
-// 			timer = time
-// 			isBig = true
-// 		},
-// 	}
-// }
-
 // define some constants
 const JUMP_FORCE = 1320
 const MOVE_SPEED = 480
