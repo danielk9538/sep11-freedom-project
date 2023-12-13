a6abca849e975032aa278799d1365b2de926192a
danielk9538 <115681045+danielk9538@users.noreply.github.com>
2023-12-13 18:04:41 +0000

    Wed, Dec 13, 2023, 1:04 PM -05:00

--- kaboom/mygame/src/main.js
+++ kaboom/mygame/src/main.js
@@ -41,22 +41,22 @@ k.add([
 	pos(12, 12),
 ])
 // custom component controlling enemy patrol movement
-// function patrol(speed = 60, dir = 1) {
-// 	return {
-// 		id: "patrol",
-// 		require: [ "pos", "area" ],
-// 		add() {
-// 			this.on("collide", (obj, col) => {
-// 				if (col.isLeft() || col.isRight()) {
-// 					dir = -dir
-// 				}
-// 			})
-// 		},
-// 		update() {
-// 			this.move(speed * dir, 0)
-// 		},
-// 	}
-// }
+function patrol(speed = 60, dir = 1) {
+	return {
+		id: "patrol",
+		require: [ "pos", "area" ],
+		add() {
+			this.on("collide", (obj, col) => {
+				if (col.isLeft() || col.isRight()) {
+					dir = -dir
+				}
+			})
+		},
+		update() {
+			this.move(speed * dir, 0)
+		},
+	}
+}
 
 // define some constants
 const JUMP_FORCE = 1000
