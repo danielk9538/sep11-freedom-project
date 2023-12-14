65e5dfd8755d440a5edf5959ff6e104e2d4b7886
danielk9538 <115681045+danielk9538@users.noreply.github.com>
2023-12-14 18:08:21 +0000

    Thu, Dec 14, 2023, 1:08 PM -05:00

--- kaboom/mygame/src/main.js
+++ kaboom/mygame/src/main.js
@@ -223,7 +223,7 @@ scene("game", ({ levelId, coins } = { levelId: 0, coins: 0 }) => {
 	// define player object
 	const player = add([
 		sprite("bean"),
-		pos(100, 200),
+		pos(0, 250),
 		area(),
 		scale(1),
 		// makes it fall to gravity and jumpable
